1f92462536c653c9ecc7b94373929f8f65105017
swoldetsadick <swoldetsadick@ineteconomics.org>
2014-09-21 20:11:14 +0200

    message

--- run_analysis.R
+++ run_analysis.R
@@ -1,7 +1,11 @@
 url <- "http://d396qusza40orc.cloudfront.net/repdata%2Fdata%2FStormData.csv.bz2"
 download.file(url, "repdata-data-StormData.csv", mode="wb")
-data <- read.csv("./repdata-data-StormData.csv", nrows=50)
-class(data)
-str(data)
 
-data <- read.csv("./repdata-data-StormData.csv",header=TRUE,
+#library(data.table)
+sample <- read.csv("./repdata-data-StormData.csv", nrows=500)
+classes <- lapply(sample, class)
+classes <- lapply(classes, function(x) ifelse(x=="integer","factor", x))
+name <- names(sample)
+str(sample)
+
+data <- read.csv("./repdata-data-StormData.csv", header=TRUE, sep= ",", col.names = name)
